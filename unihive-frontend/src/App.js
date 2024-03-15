258ade299f1551c1dc418b246917c638c0dadaca
oumaa17 <nadiroumaima17@gmail.com>
2024-03-15 10:13:52 +0000

    connecting to supabase

--- unihive-frontend/src/App.js
+++ unihive-frontend/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import supabase from "./config/supabaseClient.js"
 
 function App() {
+  console.log(supabase)
   return (
     <div className="App">
       <header className="App-header">
