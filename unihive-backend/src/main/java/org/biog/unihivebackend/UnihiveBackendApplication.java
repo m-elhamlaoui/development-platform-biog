5be9346f49ee3d1f5f59701a274dcb0a3c691c1d
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-03-26 20:59:36 +0000

    updating class diagram and adding mockups

---
+++ unihive-backend/src/main/java/org/biog/unihivebackend/UnihiveBackendApplication.java
@@ -0,0 +1,13 @@
+package org.biog.unihivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnihiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnihiveBackendApplication.class, args);
+    }
+
+}
