63ff4dd27b6b0c8d8729b6ea7e42175bdb2e35ae
oumaa17 <nadiroumaima17@gmail.com>
2024-04-16 16:20:19 +0100

    add events sidebar

--- unihive-frontend/src/pages/Events.js
+++ unihive-frontend/src/pages/Events.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
+import '../Styles/events.css';
+import Sidebar from "../components/sidebar";
 
 function Events() {
-    return (<body>
+    return (<div className="events">
         <Navbar/>
-        <h1>this is events page</h1>
+        <div className="hr-lines">
+        <div className="event-banner">
+            <h1 >Events</h1>
+</div>
+</div>
+<Sidebar/>
     <Footer/>
-    </body> ); 
+    </div> ); 
 }
 
 export default Events;
